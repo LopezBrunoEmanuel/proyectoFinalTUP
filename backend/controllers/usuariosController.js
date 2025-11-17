2780ec75fe9684f35597d3346de6734f6b164b05
LopezBrunoEmanuel <lopezbrunoemanuel@gmail.com>
2025-11-17 01:39:02 -0300

    coreccion de controller y route de usuario

--- backend/controllers/usuariosController.js
+++ backend/controllers/usuariosController.js
@@ -26,7 +26,7 @@ export const agregarUsuario = (req, res) => {
 export const editarUsuario = (req, res) => {
     const id = req.params.id
     const {nombreUsuario, emailUsuario, telefonoUsuario,direccionUsuario, rolUsuario, passwordUsuario} = req.body
-    const query = "UPDATE productos SET nombreUsuario=? , emailUsuario=?, telefonoUsuario=?, direccionUsuario=?, rolUsuario=?, passwordUsuario=? WHERE idProducto=?"
+    const query = "UPDATE usuarios SET nombreUsuario=? , emailUsuario=?, telefonoUsuario=?, direccionUsuario=?, rolUsuario=?, passwordUsuario=? WHERE idUsuario=?"
     const values = [nombreUsuario, emailUsuario, telefonoUsuario, direccionUsuario, rolUsuario, passwordUsuario, id]
 
     connection.query(query, values, (error, results) => {
@@ -37,10 +37,32 @@ export const editarUsuario = (req, res) => {
 
 export const eliminarUsuario = (req, res) => {
     const id = req.params.id
-    const query = "DELETE from usuarios WHERE idusuario=?";
+    const query = "DELETE from usuarios WHERE idUsuario=?";
 
     connection.query(query, [id], (error, results) => {
         if (error) throw error;
         res.json(results)
     })
 }
+// recuperar contraseña 
+
+export const actualizarPassword = (req, res) => {
+  console.log("Body recibido:", req.body);
+  const { emailUsuario, nuevaPassword } = req.body;
+
+  if (!emailUsuario || !nuevaPassword) {
+    return res.status(400).json({ error: "Faltan datos" });
+  }
+
+  const query = "UPDATE usuarios SET passwordUsuario = ? WHERE emailUsuario = ?";
+  connection.query(query, [nuevaPassword, emailUsuario], (error, results) => {
+    if (error) return res.status(500).json({ error: error.message });
+
+    if (results.affectedRows === 0) {
+      return res.status(404).json({ error: "Usuario no encontrado" });
+    }
+
+    res.json({ success: true, message: "Contraseña actualizada correctamente" });
+  });
+};
+
